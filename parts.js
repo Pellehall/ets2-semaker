fbfc79fda58b2ed31140f00ab9450558331cc3e0
Pellehall <pelle.hallqvist@nyckelskolan.se>
2026-04-22 09:16:48 +0200

    Create parts.js

---
+++ parts.js
@@ -0,0 +1,32 @@
+const PARTS = [
+  {
+    name: "Scania Grill",
+    slot: "f_grill",
+    data_path: "/def/vehicle/truck/scania.streamline/accessory/f_grill/shape2.sii"
+  },
+  {
+    name: "DAF Grill",
+    slot: "f_grill",
+    data_path: "/def/vehicle/truck/daf.xf/accessory/b_grill/paint.sii"
+  },
+  {
+    name: "Renault Beacon",
+    slot: "beacon",
+    data_path: "/def/vehicle/truck/renault.t/accessory/beacon/beacon_high.sii"
+  },
+  {
+    name: "Scania Exhaust Left",
+    slot: "exhaust",
+    data_path: "/def/vehicle/truck/scania.streamline/accessory/exhaust_l/side_stripe2.sii"
+  },
+  {
+    name: "Scania Exhaust Right",
+    slot: "exhaust",
+    data_path: "/def/vehicle/truck/scania.streamline/accessory/exhaust_r/side_stripe2.sii"
+  },
+  {
+    name: "Rear Light Bar",
+    slot: "r_grill",
+    data_path: "/def/vehicle/truck/scania.streamline/accessory/r_grill/shape6.sii"
+  }
+];
