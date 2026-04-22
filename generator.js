834645bd0f686b08ccfc6d68a09873b72c224001
Pellehall <pelle.hallqvist@nyckelskolan.se>
2026-04-22 09:21:34 +0200

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,12 +1,33 @@
-function generateSII(parts) {
+function generateSII(parts, lightCount) {
   let output = "";
 
   parts.forEach((p, i) => {
+
+    let slots = 1;
+    let hookups = 1;
+
+    // om det är lights → spam
+    if (p.slot === "beacon" || p.slot === "r_grill") {
+      slots = lightCount;
+      hookups = lightCount;
+    }
+
     output += `
 vehicle_addon_accessory : custom.${i} {
- slot_name: 0
- slot_hookup: 0
- data_path: "${p.data_path}"
+ slot_name: ${slots}
+`;
+
+    for (let j = 0; j < slots; j++) {
+      output += ` slot_name[${j}]: slot_${j}\n`;
+    }
+
+    output += ` slot_hookup: ${hookups}\n`;
+
+    for (let j = 0; j < hookups; j++) {
+      output += ` slot_hookup[${j}]: "smalllight1.addon_hookup"\n`;
+    }
+
+    output += ` data_path: "${p.data_path}"
 }
 `;
   });
